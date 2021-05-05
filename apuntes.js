b64c904f1890baf923b96eeb74289ecdf8a05003
pablopison <pablo.pison@gmail.com>
2021-05-04 21:58:50 -0300

    Creacion html, CSS y JS inicial

---
+++ apuntes.js
@@ -0,0 +1,14 @@
+/* Estos son algunos apuntes para las clases de DOM y eventos! */
+
+/*
+    Hay varias formas de acceder a los elementos desde la DOM API, algunas de ellas son:
+    * Por la clase del elemento (document.getElementsByClassName())
+    * Por el id del elemento (document.getElementById())
+    * Por sus selectores CSS (document.querySelector() / document.querySelectorAll())
+    * Por su etiqueta (document.getElementsByTagName())
+
+    Al "seleccionar" un elemento mediante estos métodos, recibiremos una representación del mismo que podremos
+    manipular desde JavaScript. En términos simples podemos decir que recibimos un objeto que representa el elemento HTML.
+
+    con console.dir(elemento) podemos investigar todas las propiedades y métodos que tienen estos objetos!
+*/
